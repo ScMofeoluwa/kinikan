4915e2581eaff836ba8e78016607bbbaa9fa932b
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2023-11-15 01:56:36 +0100

    refactor: make function variadic for improved flexibility

--- utils/dockercompose.go
+++ utils/dockercompose.go
@@ -13,16 +13,19 @@ type DockerCompose struct {
 	} `yaml:"services"`
 }
 
-func ExtractImagesFromCompose(filepath string) ([]string, error) {
-	if filepath == "" {
-		var err error
-		filepath, err = findBaseFile()
+func ExtractImagesFromCompose(filepath ...string) ([]string, error) {
+	var path string
+	var err error
+	if len(filepath) == 0 {
+		path, err = findBaseFile()
 		if err != nil {
 			return nil, err
 		}
+	} else {
+		path = filepath[0]
 	}
 
-	data, err := os.ReadFile(filepath)
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read docker-compose file: %w", err)
 	}
